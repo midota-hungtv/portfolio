867a7bfe37caf1f54de7de27902721cf8133db3e
Tran Van Hung <hungtranvan.bee2b@gmail.com>
2023-11-22 16:05:57 +0700

    Hide global loading

--- app.vue
+++ app.vue
@@ -1,26 +1,22 @@
 <script setup>
-import { useI18n } from 'vue-i18n'
-import { useLocaleStore } from './composables/locale'
-const { locale } = useI18n({ useScope: 'global' })
-const cookieLocale = useLocaleStore()
+import { useI18n } from "vue-i18n";
+import { useLocaleStore } from "./composables/locale";
+const { locale } = useI18n({ useScope: "global" });
+const cookieLocale = useLocaleStore();
 
-locale.value = cookieLocale.getLocale || 'en'
+locale.value = cookieLocale.getLocale || "en";
 
-const loadingPage = ref()
-
-onBeforeMount(() => {
-  loadingPage.value = false
-})
+const loadingPage = ref();
 
 onMounted(() => {
   setTimeout(() => {
-    loadingPage.value = true
-  }, 1000)
-})
+    loadingPage.value = true;
+  }, 1000);
+});
 </script>
 
 <template>
-  <LoadingPage v-show="!loadingPage" />
+  <!-- <LoadingPage v-show="!loadingPage" /> -->
 
   <NuxtLayout>
     <NuxtPage transition />
@@ -28,11 +24,13 @@ onMounted(() => {
 </template>
 
 <style>
-.v-enter-active, .v-leave-active {
+.v-enter-active,
+.v-leave-active {
   transition: opacity 0.5s ease;
 }
 
-.v-enter-from, .v-leave-to {
+.v-enter-from,
+.v-leave-to {
   opacity: 0;
 }
 </style>
