efdd31d1c67730198488a8a8cde8b45b73b4e1eb
TRAN VAN HUNG <hungtranvan.bee2b@gmail.com>
2026-03-03 11:59:08 +0700

    fix: i18n

--- plugins/i18n.ts
+++ plugins/i18n.ts
@@ -1,15 +1,19 @@
 import { createI18n } from 'vue-i18n'
+import en from '../locales/en.json'
+import idID from '../locales/id_ID.json'
 
 export default defineNuxtPlugin((nuxtApp) => {
   const locale = useCookie('locale')
+  const currentLocale = locale.value || 'en'
 
   const i18n = createI18n({
     legacy: false,
-    inheritLocale: false,
-    globalInjection: true,
-    localeDir: 'locales',
-    fallbackLocale: locale.value || 'id',
-    locale: locale.value || 'id',
+    locale: currentLocale,
+    fallbackLocale: 'en',
+    messages: {
+      en,
+      id_ID: idID
+    }
   })
 
   nuxtApp.vueApp.use(i18n)
