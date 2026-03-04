1dba3bd452d386dbafead4b05bae9bdda47619b1
TRAN VAN HUNG <hungtranvan.bee2b@gmail.com>
2026-03-04 10:29:21 +0700

    Updates

---
+++ _nuxt/CxuuBv2D.js
@@ -0,0 +1,1 @@
+import{u as s}from"./BmyV-uad.js";import{c as a,a as e,t as r,o}from"./rzL0AnPb.js";const c={class:"resume active","data-page":"resume"},l={class:"h2 article-title"},_={__name:"resume",setup(i){return s({title:"Resume"}),(t,m)=>(o(),a("article",c,[e("header",null,[e("h2",l,r(t.$t("pageTitles.resume")),1)])]))}};export{_ as default};
